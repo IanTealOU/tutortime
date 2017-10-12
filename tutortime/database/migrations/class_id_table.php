d75ecc4917d393b8bcc01657a5fc6c8dcd8f221e
JacquelineRowden <jmrowden@oakland.edu>
2017-10-11 23:13:50 -0400

    Add files via upload

---
+++ tutortime/database/migrations/class_id_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_id', function (Blueprint $table) {
+            $table->increments('class_id');
+            $table->increments('course_id');
+             $table->increments('t_id');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('level');
+    }
+}
