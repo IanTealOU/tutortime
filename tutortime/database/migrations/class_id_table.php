a1d039b2558896810c9635381033c3a967d1a719
JacquelineRowden <jmrowden@oakland.edu>
2017-10-22 11:19:18 -0400

    Updated Files: mostly in resources/views

--- tutortime/database/migrations/class_id_table.php
+++ tutortime/database/migrations/class_id_table.php
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('level');
+        Schema::dropIfExists('class_id');
     }
 }
---
+++ tutortime/resources/views/registeruser.blade.php
@@ -0,0 +1,41 @@
+<?php include('server.blade.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration for TutorTime</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Registration for TutorTime</h2>
+	</div>
+	
+	<form method="post" action="registeruser.blade.php">
+
+		<?php include('errors.blade.php'); ?>
+
+		<div class="input-group">
+			<label>User Name</label>
+			<input type="text" name="name" value="<?php echo $name; ?>">
+		</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="email" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		<div class="input-group">
+			<label>Confirm password</label>
+			<input type="password" name="password_2">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="reg_user">Register</button>
+		</div>
+		<p>
+			Already a member? <a href="login.blade.php">Sign in</a>
+		</p>
+	</form>
+</body>
+</html>
