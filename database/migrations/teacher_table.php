9b15bc892a965c29038d78ff31d029d5679f2de0
JacquelineRowden <jmrowden@oakland.edu>
2017-11-02 10:27:41 -0400

    Add files via upload

--- database/migrations/teacher_table.php
+++ database/migrations/teacher_table.php
@@ -13,12 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('s_id');
+        Schema::create('teacher', function (Blueprint $table) {
             $table->increments('t_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('teacher');
     }
 }
