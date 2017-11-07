c6122845080492d6026b6c5379f3c0c782b259eb
IanTealOU <group8OU@gmail.com>
2017-11-07 11:11:02 -0500

    Revert "database table fix"
    This reverts commit 2cccd305da536d2ad1ccae5efa77487bcb9100ab.

--- database/migrations/level_table.php
+++ database/migrations/level_table.php
@@ -13,12 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('s_id');
-            $table->increments('t_id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+        Schema::create('level', function (Blueprint $table) {
+            $table->increments('level_id');
+            $table->increments('course_id');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('level');
     }
 }
