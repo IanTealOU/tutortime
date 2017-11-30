e7a0754982efd4fe7a94a31232ede0d97ea73859
kmccloskey24 <kpmcclos@oakland.edu>
2017-11-30 11:50:13 -0500

    tutorPractice

---
+++ resources/views/adminpanel.blade.php
@@ -0,0 +1,6 @@
+<h1>Teacher Panel</h1>
+<p><a href="index.blade.php?page=admin&logout=true">Logout</a></p>
+<p><a href="index.blade.php?page=addQuestion">Add New Category</a></p>
+<p><a href="index.blade.php?page=deletecategoryselect">Delete Category</a></p>
+<p><a href="index.blade.php?page=editcategoryselect">Edit Category</a></p>
+<p><a href="index.blade.php?page=addQuestionStock">Add Question to Category</a></p>
